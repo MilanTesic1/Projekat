a03f9e392aa365f63665b34d12b1ae92b35f19cd
MilanTesic1 <125015429+MilanTesic1@users.noreply.github.com>
2023-02-14 13:01:13 +0100

    Add files via upload

---
+++ MTProjekat/script.js
@@ -0,0 +1,10 @@
+let search=document.querySelector(".search")
+
+let lupa=document.querySelector("#lupa")
+lupa.addEventListener("click",()=>{
+
+console.log("CLICK");
+search.style.width="80%";
+console.log(search);
+
+})
